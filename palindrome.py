1b31e9fa6e8ce8e549c90e2a2e336735d364368d
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-08 17:45:37 +0530

    variable name changed

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,9 @@
 x = int(input())
 rev_str = ''
-y=x
-while(y!=0 and y!=-1):
-    rem = y%10
-    y = y//10
+num=x
+while(num!=0 and num!=-1):
+    rem = num%10
+    num = num//10
     rev_str = rev_str + str(rem)
 rev_str = int(rev_str)
 print(x==rev_str)
