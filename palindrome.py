26e8dff1c6b3d11b6e37403476bb7f5b306ef799
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-08 18:14:10 +0530

    0 and -1 case

--- palindrome.py
+++ palindrome.py
@@ -5,5 +5,9 @@ while(num!=0 and num!=-1):
     rem = num%10
     num = num//10
     rev_str = rev_str + str(rem)
-rev_str = int(rev_str)
-print(x==rev_str)
+if(x==0):
+    print(True)
+elif(x==-1):
+    print(False)
+else:
+    print(x==int(rev_str))
