1fcc001daf9e331dfebf5fedba9daf53571889eb
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-08 20:53:02 +0530

    exception handled. comment outputs added.

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,38 @@
-x = int(input())
-rev_str = ''
-num=x
-while(num!=0 and num!=-1):
-    rem = num%10
-    num = num//10
-    rev_str = rev_str + str(rem)
-if(x==0):
-    print(True)
-elif(x==-1):
-    print(False)
-else:
-    print(x==int(rev_str))
+import sys,traceback
+def isPalindrome(x:int) -> bool: 
+    ##alternate approach 
+    # return str(x) == str(x)[::-1]
+    rev_str = ''
+    num = x
+    while(num != 0 and num != -1): ##loops until quotient becomes zero. At every iteration , concatenates remainder to get reverse numebr
+        rem = num % 10
+        num = num//10
+        rev_str = rev_str + str(rem)
+    if(x == 0):
+        return(True)
+    elif(x == -1):
+        return(False)
+    else:
+        return(x == int(rev_str))
+
+
+if __name__=='__main__':
+    num = int(input("Enter an Integer: "))
+    try:
+        print(isPalindrome(num))
+    except Exception as ex:
+        traceback.print_exception(*sys.exc_info())
+        print("Execution Failed")
+
+
+# Enter an Integer: 897
+# False
+
+# Enter an Integer: -121
+# False
+
+# Enter an Integer: -1
+# False
+
+# Enter an Integer: 0
+# True
