40b087006d70eed8ab4851c55d81200edc6e5617
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-11 13:12:59 +0530

    palindrome permutation of a string

---
+++ Python-leetcode/palindrome-permutation.py
@@ -0,0 +1,24 @@
+def isPalindromePermut(x: str) -> bool:
+    if(x == x[::-1]):  ##check with simple string reverse if string is already a palindrome
+        print(True)
+    else:               ##try to build a palindrome permutation if possible by comparing with reverse of string
+        arr = list(x)
+        for i in range(len(x)-1,-1,-1):
+            rev = list(arr[::-1])
+            if(arr[i] != rev[i]):
+                temp = arr[i-1]
+                arr[i-1] = arr[i]
+                arr[i] = temp
+        if(arr == arr[::-1]):
+            print(True)
+        else:
+            print(False)
+
+
+if __name__ == '__main__':
+    input_str = str(input().strip())
+    #call function
+    if(isPalindromePermut(input_str)):
+        print("Palindrome permutation possible")
+    else:
+        print("Palindrome permutation not possible")
