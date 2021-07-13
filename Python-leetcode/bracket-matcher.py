a61bc489210ef62a36805e251e9e8dc8be8743e0
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-13 10:51:44 +0530

    ##stack implement v2

--- Python-leetcode/bracket-matcher.py
+++ Python-leetcode/bracket-matcher.py
@@ -1,19 +1,22 @@
 def matchBrackets(symbols: str) -> bool:
-    symbol_stack = []
+    pair_brackets = {'(':')', '{':'}', '[':']'}
+    symbol_pair_stack = list()
+
     for bracket in symbols:
-        if bracket in symbol_stack:
-            symbol_stack.pop()
-        elif len(symbol_stack) == 0:
-            symbol_stack.append(bracket)
-        else:
-            symbol_stack.append(bracket)
-    
-    if(len(symbol_stack) != 0):
-        return False
-    else:
+        if bracket in pair_brackets.keys():
+            symbol_pair_stack.append(bracket)
+        elif bracket in pair_brackets.values():
+            if len(symbol_pair_stack) == 0:
+                return False
+            else:
+                if(pair_brackets[symbol_pair_stack.pop()] != bracket):
+                    return False
+    if(len(symbol_pair_stack) == 0):
         return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
-    input_symbols = str(input())
+    input_symbols = str(input().strip())
     print(matchBrackets(input_symbols))
