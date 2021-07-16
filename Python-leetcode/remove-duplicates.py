31c7762802c26cc8c38c3ea6e817f5b5a7381be7
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2021-07-17 00:05:12 +0530

    correction

--- Python-leetcode/remove-duplicates.py
+++ Python-leetcode/remove-duplicates.py
@@ -1,7 +1,11 @@
-def removeElement(self, nums: List[int], val: int) -> int:
+def removeElement(nums: list, val: int) -> int:
         index = 0
         for i in range(0,len(nums)):
             if(nums[i] != val):
                 nums[index] = nums[i]
                 index+=1
         return index
+
+val = int(input())
+nums = list(map(int,input().strip().split()))
+removeElement(nums,val)
