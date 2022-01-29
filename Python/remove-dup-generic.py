f89c2855767a974d4fbba0433518e4eebda290c1
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2022-01-29 13:26:25 +0530

    formatting changes pylint

--- Python/remove-dup-generic.py
+++ Python/remove-dup-generic.py
@@ -1,17 +1,20 @@
-## Note This implementation will work for both sorted and non-sorted arrays. It doesn't matter if the nums 
-##array is in non-decreasing or non-increasing order
+'''
+Note This implementation will work for both sorted and non-sorted arrays.
+It doesn't matter if the nums
+array is in non-decreasing or non-increasing order
+'''
 nums = list(map(int, input().strip().split()))
-current = 0
-ahead = 1
-dup = 0
+CURRENT = 0
+AHEAD = 1
+DUP = 0
 n = len(nums)
-while(ahead < n):
-    if(nums[current] == nums[ahead]): ##check if duplicate and keep track of duplicates.
-        dup += 1
-        ahead += 1
-    elif(nums[current] != nums[ahead]):  ##for next non-duplicate 
-        if(dup != 0):
-            nums[current + 1] = nums[ahead]
-        current += 1
-        ahead += 1
-print(current + 1)
+while AHEAD < n:
+    if nums[CURRENT] == nums[AHEAD]: ##check if duplicate and keep track of DUPlicates.
+        DUP += 1
+        AHEAD += 1
+    elif nums[CURRENT] != nums[AHEAD]: ##for next non-duplicate
+        if DUP != 0:
+            nums[CURRENT + 1] = nums[AHEAD]
+        CURRENT += 1
+        AHEAD += 1
+print(CURRENT + 1)
