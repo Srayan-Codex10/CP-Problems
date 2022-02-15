d593ce0584b1ce6a1a25aeb1008b97006e728c10
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2022-02-15 23:58:27 +0530

    subarray sum brute force

--- C++/arrays-test.cpp
+++ C++/arrays-test.cpp
@@ -1,3 +1,6 @@
+/***
+ * @author Srayan Ray
+ * */
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -8,16 +11,41 @@ void testPrintArray(int *arr)
     // cout << sizeof(arr) << endl; //prints size of pointer to array ; 8 bytes
     int n = sizeof(arr) / sizeof(int);
     arr[0] = 100;
-    cout << arr; //prints base address of arr
+    cout << arr; // prints base address of arr
     for (int i = 0; i < n; ++i)
     {
         cout << arr[i] << endl;
     }
 }
 
+// Brute Force
+int subArraySum(int *arr, int n)
+{
+    int largestSum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            int subArraySum = 0;
+            for (int k = i; k <= j; k++)
+            {
+                subArraySum += arr[k];
+            }
+            cout << "Current Sum: " << subArraySum << endl;
+            largestSum = max(subArraySum, largestSum);
+        }
+    }
+
+    return largestSum;
+}
+
 int main()
 {
+    int arr[] = {1, 2, 3, 5, 8};
+    int s = sizeof(arr) / sizeof(int);
 
+    cout << subArraySum(arr, s) << endl;
     /*  int marks[100] = {0}; //initialize //create
 
     int n;
@@ -34,10 +62,10 @@ int main()
         cout << marks[i] << ", "; //output
     } */
 
-    //int arr[] = {1, 2, 3, 4, 5};
-    //int *a = arr; // arr is a address variable with the base address of the array
-    // cout << a << endl; // will print base address of array.
-    // cout << sizeof(arr) << endl; // prints total size of array with each integer as 4 bytes
+    // int arr[] = {1, 2, 3, 4, 5};
+    // int *a = arr; // arr is a address variable with the base address of the array
+    //  cout << a << endl; // will print base address of array.
+    //  cout << sizeof(arr) << endl; // prints total size of array with each integer as 4 bytes
     /*  testPrintArray(a);
     int num_el = sizeof(arr) / sizeof(int);
     for (int i = 0; i < num_el; ++i)
