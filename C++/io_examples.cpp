ca8065c81a0e3be92a60c34db99c12bc69556056
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2022-04-03 16:20:42 +0530

    shortest path with strings

--- C++/io_examples.cpp
+++ C++/io_examples.cpp
@@ -39,8 +39,102 @@ int countChars()
     return 0;
 }
 
+/**
+ * !!Problem:
+ *  Give a string of paths as SWNE , [South, West, North , East] in 2-D coordinate,
+ *  print the shortest path between initial and final position.
+ *  Example: SNNNEWE
+ *  Output: NNE  [x=1, y=2]
+ * */
+int printShortestRoute()
+{
+
+    char path[1000];
+    cin.getline(path, 1000);
+
+    int x = 0, y = 0;
+    for (int i = 0; path[i] != '\0'; i++)
+    {
+        switch (path[i])
+        {
+
+        case 'S':
+            y--;
+            break;
+
+        case 'N':
+            y++;
+            break;
+
+        case 'E':
+            x++;
+            break;
+
+        case 'W':
+            x--;
+            break;
+
+        default:
+            cout << "Invalid Route";
+        }
+    }
+
+    cout << "X: " << x << ", "
+         << "Y: " << y << endl;
+
+    if (x >= 0 && y >= 0)
+    {
+        while (y--)
+        {
+            cout << 'N';
+        }
+        while (x--)
+        {
+            cout << 'E';
+        }
+    }
+    else if (x <= 0 && y >= 0)
+    {
+        while (y--)
+        {
+            cout << 'N';
+        }
+        while (x++)
+        {
+            cout << 'W';
+        }
+    }
+    else if (x <= 0 && y <= 0)
+    {
+        while (y++)
+        {
+            cout << 'S';
+        }
+        while (x++)
+        {
+            cout << 'W';
+        }
+    }
+    else if (x >= 0 && y <= 0)
+    {
+        while (y++)
+        {
+            cout << 'S';
+        }
+        while (x--)
+        {
+            cout << 'E';
+        }
+    }
+
+    return 0;
+}
+
 int main()
 {
+
+    printShortestRoute();
+
     char line[1000];
 
     char temp;
