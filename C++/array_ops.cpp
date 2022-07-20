6ea41580f96764f2da4d61078e873142335f6aec
Srayan Ray <37058781+Srayan-Codex10@users.noreply.github.com>
2022-07-20 22:55:37 +0530

    array insert,delete

--- C++/array_ops.cpp
+++ C++/array_ops.cpp
@@ -1,30 +1,97 @@
 #include <iostream>
+#define N 20
+
 using namespace std;
 
 // Traverse Array
-void printArray(int *arr, int n)
+void printArray(int *arr, int size)
 {
-
-    for (int i = 0; i < n; i++)
+    cout << endl;
+    for (int i = 0; i <= size; i++)
     {
         cout << arr[i] << " ";
     }
 }
 
-// Insert at end of array
-int push(int *arr, int &upper_b, int n, int data)
+// delete from end
+int pop(int *arr, int &upper_bound)
 {
-    if (upper_b == n)
+    if (upper_bound != 0)
+    {
+        upper_bound--;
+    }
+    else
+    {
+        cout << endl;
+        cout << "Array is Empty" << endl;
+        return -1;
+    }
+    printArray(arr, upper_bound);
+    return 0;
+}
+
+int remove(int *arr, int pos, int &upper_bound)
+{
+    if (upper_bound == 0)
+        return -1;
+
+    if (pos == upper_bound)
+    {
+        pop(arr, upper_bound);
+    }
+    else
+    {
+        // left shifting elements starting from delete index
+        for (int i = pos; i < upper_bound; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+    }
+    upper_bound--;
+    printArray(arr, upper_bound);
+    return 0;
+}
+
+// Insert at end of array
+int push(int *arr, int &upper_b, int data)
+{
+    if (upper_b == N - 1)
         return -1;
 
     arr[upper_b + 1] = data;
     upper_b++;
+    return 0;
+}
+
+// Insert at index i
+int add(int *arr, int &n, int data, int idx)
+{
+    if (n == N - 1)
+        return -1;
+
+    if (idx == n)
+        return -1;
+
+    // right shifting the elements after insert position.
+    for (int i = n; i >= idx; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    cout << endl;
+    // printArray(arr, n);
+    arr[idx] = data;
+    n++;
+
+    return 0;
+}
+
+// Read from File
+void read_data(int *arr, int n)
+{
 }
 
 int main()
 {
-    int data;
-    int N = 20;
     int arr[N] = {0};
     int last = 0;
 
@@ -36,6 +103,21 @@ int main()
         arr[i] = i + 1;
         last = i;
     }
+    printArray(arr, last);
+    cout << endl;
+    cout << last << endl;
 
-    
+    push(arr, last, 23);
+    printArray(arr, last);
+    cout << endl
+         << last;
+
+    add(arr, last, 43, 3);
+    printArray(arr, last);
+
+    pop(arr, last);
+    pop(arr, last);
+    remove(arr, 4, last);
+
+    return 0;
 }
